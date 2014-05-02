429507a242eb0005efa6c4c51a433f9b0893014e
phynalle <phynalism@gmail.com>
2014-05-03 04:36:00 +0900

    changed intro html templates (simplified complex hierarchy, added normalize.css to make layout modern, and separated css from html code. and requirements.txt added. third parties required should be added in it for us to know easily.

--- website/website/urls.py
+++ website/website/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from intro.views import main
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dinger.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('intro.urls')),
+    url(r'^$', main),
+    url(r'^intro/', include('intro.urls')),
     url(r'^member/', include('member.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
