4bfec061b93bad2f4f99247078bb4fe1ecf260c6
phynalle <phynalism@gmail.com>
2014-07-23 03:37:08 +0900

    added missing a css file [album.css], album.html is modified.

--- website/album/views.py
+++ website/album/views.py
@@ -55,10 +55,7 @@ def thumbnail(request, photo_id):
 
 @login_required
 def album_main(request):
-    photos = reversed(Photo.objects.all())
-    
     return render(request, 'album.html', {
-        'photos': grouper(4, photos),
     })
 
     
@@ -179,31 +176,3 @@ def ajax_request_photos(request):
 
     response_dict.update({'status':0, 'result': result_dict})
     return AjaxJsonResponse(response_dict)
-    
-    
-def ajax_request_photo(request):
-    if not request.POST:
-        return render_to_response('weblog/ajax_example.html', {})
-    response_dict = {}
-    name = request.POST.get('name', False)
-    total = request.POST.get('total', False)
-    response_dict.update({'name': name, 'total': total})
-    if total:
-        try:
-            total = int(total)
-        except:
-            total = False
-    if name and total and int(total) == 10:
-        response_dict.update({'success': True })
-    else:
-        response_dict.update({'errors': {}})
-        if not name:
-            response_dict['errors'].update({'name': 'This field is required'})
-        if not total and total is not False:
-            response_dict['errors'].update({'total': 'This field is required'})
-        elif int(total) != 10:
-            response_dict['errors'].update({'total': 'Incorrect total'})
-    return render_to_response('weblog/ajax_example.html', response_dict)
-
-    return AjaxJsonResponse(data)
-    
